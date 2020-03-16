f9c9b3af035b92060023e5cbd419253c937a6e6e
root <ruelosjoel@gmail.com>
2020-03-16 01:44:27 -0500

    Added BinaryTreeTraversal - DFS / BFS

--- src/main/java/org/jrue/dsalgo/btree/BinaryTreeTraversal.java
+++ src/main/java/org/jrue/dsalgo/btree/BinaryTreeTraversal.java
@@ -1,4 +1,80 @@
 package org.jrue.dsalgo.btree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeTraversal {
+
+    private BinaryTreeImpl tree;
+
+    public BinaryTreeTraversal(BinaryTreeImpl tree) {
+        this.tree = tree;
+    }
+
+    public String inOrder() {
+        return inOrderRecursive(tree.getRoot(), new StringBuilder()).toString();
+    }
+
+    public String preOrder()  {
+        return preOrderRecursive(tree.getRoot(), new StringBuilder()).toString();
+    }
+
+    public String postOrder() {
+        return postOrderRecursive(tree.getRoot(), new StringBuilder()).toString();
+    }
+
+    private StringBuilder inOrderRecursive(Node<?> node, StringBuilder result) {
+        if(node == null) {
+            return result;
+        }
+
+        inOrderRecursive(node.getLeft(), result);
+        result.append(node.getValue() + " ");
+        inOrderRecursive(node.getRight(), result);
+        return result;
+    }
+
+    private StringBuilder preOrderRecursive(Node<?> node, StringBuilder result) {
+        if(node == null) {
+            return result;
+        }
+
+        result.append(node.getValue() + " ");
+        preOrderRecursive(node.getLeft(), result);
+        preOrderRecursive(node.getRight(), result);
+        return result;
+    }
+
+    private StringBuilder postOrderRecursive(Node<?> node, StringBuilder result) {
+        if(node == null) {
+            return result;
+        }
+
+        postOrderRecursive(node.getLeft(), result);
+        postOrderRecursive(node.getRight(), result);
+        result.append(node.getValue() + " ");
+        return result;
+    }
+
+    public String levelOrder() {
+        StringBuilder sb = new StringBuilder();
+        Queue<Node> nodes = new LinkedList<>();
+        nodes.add(tree.getRoot());
+        while(nodes.peek() != null) {
+
+            Node node = nodes.poll();
+            if(node.getLeft() != null) {
+                nodes.add(node.getLeft());
+            }
+
+            if(node.getRight() != null) {
+                nodes.add(node.getRight());
+            }
+
+            sb.append(node.getValue() + " ");
+
+        }
+
+        return sb.toString();
+    }
 }
---
+++ src/test/java/org/jrue/dsalgo/btree/TestBinaryTreeTraversal.java
@@ -0,0 +1,47 @@
+package org.jrue.dsalgo.btree;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+
+@RunWith(JUnit4.class)
+public class TestBinaryTreeTraversal {
+
+    private BinaryTreeTraversal traversal;
+    private BinaryTreeImpl tree;
+
+    @Before
+    public void setup() {
+        this.tree = new BinaryTreeImpl(50);
+        this.traversal = new BinaryTreeTraversal(tree);
+        tree.add(60);
+        tree.add(55);
+        tree.add(65);
+        tree.add(40);
+        tree.add(30);
+        tree.add(45);
+    }
+
+    @Test
+    public void testInOrderTraversal() {
+        assertEquals("30 40 45 50 55 60 65", traversal.inOrder().trim());
+    }
+
+    @Test
+    public void testPreOrderTraversal() {
+        assertEquals("50 40 30 45 60 55 65", traversal.preOrder().trim());
+    }
+
+    @Test
+    public void testPostOrderTraversal() {
+        assertEquals("30 45 40 55 65 60 50", traversal.postOrder().trim());
+    }
+
+    @Test
+    public void testLevelOrderTraversal() {
+        assertEquals("50 40 60 30 45 55 65", traversal.levelOrder().trim());
+    }
+}
